3346bc928bdc7fdc9c127dbee303ff5012c3a193
Pedro Duarte <contact@peduarte.com>
2015-08-19 19:23:28 +0100

    Improve setup

---
+++ src/index.js
@@ -0,0 +1,48 @@
+/**
+* mdhtmljson
+*
+* @fileoverview Convert markdown to html into a json file
+*
+* @author Pedro Duarte
+* @author http://pedroduarte.me
+*
+*/
+
+'use strict';
+
+var fs = require('fs');
+var request = require('request');
+var marked = require('marked');
+
+module.exports = init;
+
+function init(url, output) {
+  /**
+   * if there is no output, set default path and filename
+   * @param  {string} !output output of json
+   * @return {string}         default output including .json extension if not set
+   */
+  if (!output) {
+    console.log('no output -> ');
+    output = './mdhtml.json';
+    output = output.indexOf('.json') > -1 ? output : output + '.json';
+  }
+
+  parseMarkdown(url, output);
+}
+
+function parseMarkdown(url, output) {
+  request(url, function (error, response, html) {
+    if (!error && response.statusCode == 200) {
+      var parsedResult = {
+        html: marked(response.body)
+      };
+
+      fs.writeFile(output, JSON.stringify(parsedResult, null, 4), function(error){
+        if (error) { return console.log('Ooops, we have an error – Are you sure the output directory exists? ' + error); }
+        console.log('Your file is ready – ' + output);
+      });
+
+    }
+  });
+}
