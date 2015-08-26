868957926ba98d529f509f27b7f26f56f94f5bf4
Pedro Duarte <contact@peduarte.com>
2015-08-26 21:54:51 +0100

    Fix bug with the cmd

--- src/mdhtmljson.js
+++ src/mdhtmljson.js
@@ -8,44 +8,55 @@
 *
 */
 
-'use strict';
+(function(global){
 
-var fs = require('fs');
-var request = require('request');
-var marked = require('marked');
+  'use strict';
 
-module.exports = Mdhtmljson;
+  var fs = require('fs');
+  var request = require('request');
+  var marked = require('marked');
 
-function Mdhtmljson(url, output) {
-  // if there is no output, set default path and filename
-  if (!output) { this.output = './mdhtml.json'; }
+  function Mdhtmljson(url, output) {
+    // if there is no output, set default path and filename
+    if (!output) { output = './mdhtml.json'; }
 
-  // if it doesn't end with .json, add it
-  this.output = this.output.match('\.json$') ? this.output : this.output + '.json';
+    // if it doesn't end with .json, add it
+    this.output = output.match('\.json$') ? output : output + '.json';
 
-  this.parseMarkdown(url, this.output);
-}
+    this.parseMarkdown(url, this.output);
+  }
 
-Mdhtmljson.prototype.parseMarkdown = function(url, output) {
-  request(url, function (error, response, html) {
-    if (error) {
-      console.log(error + ' :(');
-      return false;
-    }
+  Mdhtmljson.prototype.parseMarkdown = function(url, output) {
+    request(url, function (error, response, html) {
+      if (error) {
+        console.log(error + ' :(');
+        return false;
+      }
 
-    if (response.statusCode == 200) {
-      var parsedResult = {
-        html: marked(response.body)
-      };
+      if (response.statusCode == 200) {
+        var parsedResult = {
+          html: marked(response.body)
+        };
 
-      fs.writeFile(output, JSON.stringify(parsedResult, null, 4), function(error){
-        if (error) { return console.log('Ooops, are you sure the output directory exists? ' + error); }
-        console.log('Your file is ready – ' + output);
-      });
-    }
+        fs.writeFile(output, JSON.stringify(parsedResult, null, 4), function(error){
+          if (error) { return console.log('Ooops, are you sure the output directory exists? ' + error); }
+          console.log('Your file is ready – ' + output);
+        });
+      }
 
-    else {
-      console.log('Something went wrong, response status code ', response.statusCode);
-    }
-  });
-}
+      else {
+        console.log('Something went wrong, response status code ', response.statusCode);
+      }
+    });
+  }
+
+  // Exports to multiple environments
+  if(typeof define === 'function' && define.amd){
+    define(function () { return Mdhtmljson; });
+  } else if (typeof module !== 'undefined' && module.exports){
+    module.exports = Mdhtmljson;
+  } else {
+    global['Mdhtmljson'] = Mdhtmljson;
+  }
+
+}(this));
